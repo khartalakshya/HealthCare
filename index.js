8ffe6d2af607558ab1b710ae6de982f9b637c593
lakshya gota <lakshyagota256@gmail.com>
2025-03-30 12:24:10 +0530

    modified  version

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import express from "express";
 const app= express();
 app.get("/",(req,res)=>
 {
-    res.send("hello team");
+    res.send("hello team how are you");
 });
 
 app.listen(3000,()=>
 {
-    console.log("serever is running in port 3000:");
+    console.log("serever is running in port 3000");
 });
